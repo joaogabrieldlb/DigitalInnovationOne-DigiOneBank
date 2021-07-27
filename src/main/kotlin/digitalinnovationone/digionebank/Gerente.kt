6e7c081d0e2758b7e9271315f8fef17213eb469f
joaogabrieldlb <joaogabrieldlb@hotmail.com>
2021-07-27 17:20:56 -0300

    Updated Gerente.kt and test to it

--- src/main/kotlin/digitalinnovationone/digionebank/Gerente.kt
+++ src/main/kotlin/digitalinnovationone/digionebank/Gerente.kt
@@ -3,9 +3,10 @@ package digitalinnovationone.digionebank
 class Gerente(
     nome: String,
     cpf: CPF,
-    salario: Double
-) : Funcionario(nome = nome, cpf = cpf, salario = salario) {
+    salario: Double,
+    val senha: String
+) : Funcionario(nome = nome, cpf = cpf, salario = salario), Logavel {
     override fun calculoAuxilio() = salario * 0.4
 
-
+    override fun login(): Boolean = "senha123" == senha
 }
--- src/main/kotlin/digitalinnovationone/digionebank/testes/TesteFuncionarioGerente.kt
+++ src/main/kotlin/digitalinnovationone/digionebank/testes/TesteFuncionarioGerente.kt
@@ -4,6 +4,7 @@ import digitalinnovationone.digionebank.CPF
 import digitalinnovationone.digionebank.Gerente
 
 fun main() {
-    val maria = Gerente(nome = "Maria Eduarda",cpf = CPF("000.000.000-00"), salario = 4000.0)
+    val maria = Gerente(nome = "Maria Eduarda",cpf = CPF("000.000.000-00"), salario = 4000.0, senha = "senha123")
     ImprimeRelatorioFuncionario.imprime(maria)
+    TesteAutenticacao().autentica(maria)
 }
