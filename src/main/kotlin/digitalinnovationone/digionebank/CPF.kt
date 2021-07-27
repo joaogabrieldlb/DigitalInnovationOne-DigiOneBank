725169c3c8461cb180e6391748739a790659dd2c
joaogabrieldlb <joaogabrieldlb@hotmail.com>
2021-07-26 22:40:56 -0300

    Created CPF class for person

---
+++ src/main/kotlin/digitalinnovationone/digionebank/CPF.kt
@@ -0,0 +1,20 @@
+package digitalinnovationone.digionebank
+
+class CPF(private val cpf: String) {
+
+//    private var cpf: String
+//
+//    constructor(cpf: String) {
+//        this.cpf = cpf
+//    }
+
+//    init {
+//        if(isCPF(cpf))
+//        this.cpf = cpf
+//    }
+
+//    private isCPF(cpf: String): Boolean {
+//        return false
+//    }
+    
+}
