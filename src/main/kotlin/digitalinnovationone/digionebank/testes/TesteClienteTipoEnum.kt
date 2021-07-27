51eb64f2e80be3986b43062305e30260a9b24a1b
joaogabrieldlb <joaogabrieldlb@hotmail.com>
2021-07-26 23:02:04 -0300

    Updated ClienteTipo.kt and test to it

--- src/main/kotlin/digitalinnovationone/digionebank/testes/TesteClienteTipoEnum.kt
+++ src/main/kotlin/digitalinnovationone/digionebank/testes/TesteClienteTipoEnum.kt
@@ -4,6 +4,12 @@ import digitalinnovationone.digionebank.ClienteTipo
 
 fun main() {
     ClienteTipo.values().forEach {
-        println("${it.name} = $it")
+        println("${it.name} = ${it.descricao}")
     }
+
+    val pf = ClienteTipo.PF
+    println("${pf.name} = ${pf.descricao}")
+
+    val pj = ClienteTipo.PJ
+    println("${pj.name} = ${pj.descricao}")
 }
